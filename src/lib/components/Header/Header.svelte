4dad6ef225c5a9592858bbcd82ca2a189949c53c
Mees Dekker <meesdekker2622002@gmail.com>
2022-07-27 14:08:49 +0200

    added login logout switcher

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Logo from './Logo.svelte';
-	import { page } from '$app/stores';
+	import { store as authStore, logout } from '$lib/stores/auth';
+
+	$: user = $authStore;
+	$: console.log(user);
 </script>
 
 <nav class="flex items-center justify-between max-w-3xl p-4 mx-auto">
@@ -22,7 +25,8 @@
 		</li>
 
 		<li>
-			<a sveltekit:prefetch class="inline-flex items-center px-3 py-2 rounded-lg" href="/login">
+			{#if !user}
+				<a sveltekit:prefetch class="inline-flex items-center px-3 py-2 rounded-lg" href="/login">
 				Login
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -39,6 +43,26 @@
 					/>
 				</svg>
 			</a>
+			{:else}
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<a class="inline-flex items-center px-3 py-2 rounded-lg" href="javascript:void(0)" on:click|preventDefault={logout}>
+				Logout
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					class="ml-1.5 w-4 h-4"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+					/>
+				</svg>
+			</a>
+			{/if}
 		</li>
 	</ul>
 </nav>
